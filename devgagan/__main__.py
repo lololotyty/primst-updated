3832197829e98cbcd889a6d36145f89e20da64f2
wddwwdw <hangerskype@gmail.com>
2025-02-11 03:03:47 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -41,9 +41,9 @@ async def devggn_boot():
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
 👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+🌐 GitHub: https://telegram.dog/shimps_bot
+📬 Telegram: https://telegram.dog/shimps_bot
+▶️ YouTube: https://telegram.dog/shimps_bot
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
