274ade6c80c0d7e1c0e2289c97790b4edd2ea8d8
wddwwdw <hangerskype@gmail.com>
2025-03-24 14:24:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "22430866"))
+API_HASH = getenv("API_HASH", "20a0f0284c49fe38bb3676c297fbf947")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6820248376").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8028959557").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "-1002226652008")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002216382990"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "50000"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "1000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshr")
 AD_API = getenv("AD_API", "52b4a2cf4687d8f2b7bc2943f618e78cb")
 STRING = getenv("STRING", None)
